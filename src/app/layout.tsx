ec3e7e3f223ce9aeb39617092f03cf57052cc4c2
Fatin Nabila <fatinabila49@gmail.com>
2025-11-09 23:52:07 +0800

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.scss'
-import Navbar from "./components/navbar";
-import Sidebar from "./components/sidebar";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const geistSans = Geist({
